83331d0ef641655ff6a6b0805f7c8ed1773a0a5a
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-10-16 15:56:06 -0700

    update to practices

--- all-del-prac-probs.rb
+++ all-del-prac-probs.rb
@@ -133,7 +133,7 @@ puts "These cities are in California " + city1 + ", " + city2 + " and " + city3 
 # 1 Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
 number = 10
-if number = 10
+if number == 10
   puts 0
 else
   puts -1
