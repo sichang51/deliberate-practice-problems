b3039c266ce0706b38862f77fd41d779d4a48d49
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-10-30 15:57:08 -0700

    exercising

---
+++ 01-variables1v3.rb
@@ -0,0 +1,54 @@
+# 1 Write a program that uses a variable to store a number, then prints that number plus 10.
+
+number = 5
+puts number + 10
+
+# 2 Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = "shotcaller"
+puts word.upcase
+
+# 3 Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+number1 = 50
+number2 = 1
+
+puts number1 + number2
+
+# 4 Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+word = "scramble"
+puts word.reverse
+
+# 5 Write a program that uses a variable to store a number, then prints the number times 10.
+
+number = 10
+puts number * 10
+
+# 6 Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "shot"
+word2 = "caller"
+
+puts word1.upcase + word2.upcase
+
+# 7 Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word = "javascript"
+puts word.length
+
+# 8 Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+number = 5.586
+puts number.to_i
+
+# 9 Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+number1 = 5
+number2 = 10
+puts number1 * number2
+
+#10 Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+word = "PYTHON"
+puts word.downcase
