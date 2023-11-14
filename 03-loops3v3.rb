b2b313f7e4cd877b17b64926b59b7ff937b639cd
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-13 16:05:31 -0800

    answered two questions

--- 03-loops3v3.rb
+++ 03-loops3v3.rb
@@ -1,3 +1,26 @@
 # 1 Start with an array of numbers and create a new array with only the numbers less than 20.
-For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+# For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
+numbers = [2, 32, 80, 18, 12, 3]
+low_numbers = []
+index = 0
+while index < numbers.length
+  number = numbers[index]
+  if number < 20
+    low_numbers << number
+  end
+  index += 1
+end
+p low_numbers
+
+def numbers(array)
+  low_number = []
+  array.each do |number|
+    if number < 20
+      low_number << number
+    end
+  end
+  return low_number
+end
+
+p numbers([2, 32, 80, 18, 12, 3])
